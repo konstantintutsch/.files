6947fc299b4bbfd8ee8e01d8a4389d5fe80e61ba
Konstantin Tutsch <mail@konstantintutsch.com>
2025-12-20 14:39:43 +0100

    feat: add vimtex plugin
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

---
+++ after/plugin/vimtex.lua
@@ -0,0 +1,6 @@
+-- viewing
+vim.g.vimtex_view_general_viewer = "flatpak"
+vim.g.vimtex_view_general_options = "run org.gnome.Papers file:@pdf#src:@line@tex"
+
+-- compiling
+vim.g.vimtex_compiler_method = "pdflatex"
