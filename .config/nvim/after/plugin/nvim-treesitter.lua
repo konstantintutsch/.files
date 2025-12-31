7c0fb96d26948947c11661b1eac9e8a0ad0abefd
Konstantin Tutsch <mail@konstantintutsch.com>
2025-12-31 16:10:51 +0100

    fix: nvim-treesitter.configs not found
    nvim-treesitter was updated to use nvim-treesitter instead of
    nvim-treesitter.configs
    
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter'.setup {
     ensure_installed = {"c", "lua", "python", "bash", "make", "meson", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "gpg", "just", "blueprint", "html", "css", "markdown", "latex"},
     auto_install = true,
 
